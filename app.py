d608d59442d4b092e3a74eeb75d6fc32bcd81574
Theriley106 <christopherlambert106@gmail.com>
2019-03-30 11:41:31 -0700

    Updated

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from twilio.rest import Client
 from keys import *
 import requests
 import search
+import createInventory
 
 CALL_LIST = []
 CALL_LOG = {}
@@ -45,7 +46,39 @@ def testPage():
 	elif intent == "closestStore":
 		return jsonify(genSpeech('Your closest store is ' + search.store_by_zip(CALL_LOG[sessionID]['FromZip'])['name']))
 	elif intent == 'buy':
-		return jsonify(genSpeech("I see you're trying to buy a {}".format(parameters['phone'])))
+		phone = parameters['phone']
+		memory = parameters.get("memory", None)
+		sku = createInventory.get_sku_by_mem_phone(phone, memory)
+		#print CALL_LOG[sessionID]
+		#raw_input("WAITING")
+		stores = [x['id'] for x in search.store_by_zip(CALL_LOG[sessionID].get('FromZip'))]
+		stockInfo = search.check_stock(sku, stores)
+		found = False
+		returnVal = "Unfortunately, the device is not in stock at any stores near your location."
+		for val in stockInfo.get('result', {}).get('inventoryAvailabilityList', []):
+			try:
+				s = val['storeId']
+				q = val['skuDetails'][0]['quantity']['availableQuantity']
+				print("Checking Store ID: {} | Availability: {}".format(s, q))
+			except:
+				pass
+
+		for val in stockInfo.get('result', {}).get('inventoryAvailabilityList', []):
+			try:
+				s = val['storeId']
+				q = val['skuDetails'][0]['quantity']['availableQuantity']
+				print("Checking Store ID: {} | Availability: {}".format(s, q))
+				if int(q) > 0:
+					found = True
+					print("FOUND CLOSEST STORE WITH SKU: {} IN STOCK: Store {}".format(sku, s))
+					break
+			except:
+				pass
+		if found:
+			fVal = search.get_info_store(CALL_LOG[sessionID].get('FromZip'), s)
+			returnVal = "{} has the {} in stock.  I will transfer you over to a store representative from the {} store now.  ".format(fVal, createInventory.get_phone_info_with_sku(sku), fVal.partition("The store on ")[2].partition(" in ")[0])
+		print("customer is looking for SKU: {}".format(sku))
+		return jsonify(genSpeech(returnVal))
 	elif intent == 'payBill':
 		return jsonify(genSpeech("Bill has been successfully paid.  We have sent you a text message will your payment receipt.  Note that this message will not count towards your text allowance.  Is there anything else I can help you with today?"))
 	else:
--- createInventory.py
+++ createInventory.py
@@ -4,27 +4,41 @@ import re
 
 a = json.load(open("inventory.json"))
 
-def get_sku_by_mem_phone(phone, memory):
+def get_sku_by_mem_phone(phone, memory=None):
 	for val in a['values']['products']:
 		if val["familyName"].encode('ascii',errors='ignore').lower().replace("+", " plus") == phone or val["productName"].encode('ascii',errors='ignore').lower().replace("+", " plus") == phone:
-			memoryIndex = re.findall("\d+", val["deviceMemorySKU"]).index(memory)
+			if memory == None:
+				return val['skuCode'].split("|")[0]
+			allVals = re.findall("\d+", val["deviceMemorySKU"])
+			print allVals
+			if str(memory) in allVals:
+				memoryIndex = allVals.index(memory)
+			else:
+				memoryIndex = 0
 			sku = val['skuCode'].split("|")[memoryIndex]
 			return sku
 
+def get_phone_info_with_sku(skuNumber):
+	for val in a['values']['products']:
+		if str(skuNumber) in str(val):
+			return val["productDisplayName"]
+
+
 if __name__ == '__main__':
-	print get_sku_by_mem_phone("lg stylo 4", "32")
-	raw_input(" ")
+	#print get_sku_by_mem_phone("lg stylo 4")
+	#raw_input(" ")
 	for v in a['values']['products']:
 		phone = v["familyName"].encode('ascii',errors='ignore').lower().replace("+", " plus")
 		if len(phone) == 0:
 			phone = v["productName"].encode('ascii',errors='ignore').lower().replace("+", " plus")
 		memory = re.findall("\d+", v["deviceMemorySKU"])
-		for val in memory:
-			if int(val) == 1:
-				print("{} {}".format(val, phone))
-				print("{} terabyte {}".format(val, phone))
-				print("{} terabyte {}".format(val, phone))
-			else:
-				print("{} {}".format(val, phone))
-				print("{} gigabyte {}".format(val, phone))
-				print("{} {} gigabyte".format(phone, val))
+		print("\"{}\", \"{}\"".format(phone, phone))
+		"""for val in memory:
+									if int(val) == 1:
+										print("{} {}".format(val, phone))
+										print("{} terabyte {}".format(val, phone))
+										print("{} terabyte {}".format(val, phone))
+									else:
+										print("{} {}".format(val, phone))
+										print("{} gigabyte {}".format(val, phone))
+										print("{} {} gigabyte".format(phone, val))"""
